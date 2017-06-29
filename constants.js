239e75ebcab81111d4fae020ca11a6eaa87b532a
nhatdq1810 <dangquangnhat18101994@gmail.com>
2017-06-29 17:58:51 +0700

    change method to post, pass issue type for request jira

--- constants.js
+++ constants.js
@@ -2,7 +2,9 @@ var username = 'Nhat.Dang';
 var pwd = 'N@025104686/';
 var host = 'jira.hotschedules.com';
 var port = 80;
+var project = 'NGINV';
+var fields = ['issuetype', 'description', 'issuelinks', 'priority', 'status', 'summary'];
 
-var constants = { username, pwd, host };
+var constants = { username, pwd, host, project, fields };
 
 module.exports = constants;
--- routes/issues.js
+++ routes/issues.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var router = express.Router();
-var constants = require('../constants');
+const express = require('express');
+const router = express.Router();
+const constants = require('../constants');
 
-var JiraClient = require('jira-connector');
+const JiraClient = require('jira-connector');
 
-var jira = new JiraClient({
+const jira = new JiraClient({
   host: constants.host,
   basic_auth: {
     username: constants.username,
@@ -12,11 +12,80 @@ var jira = new JiraClient({
   }
 });
 
-router.get('/', function (req, res, next) {
-  jira.search.search({jql: 'project=NGINV'}, function (error, issue) {
-    console.log(error);
-    res.send({ issue });
+function countIssues() {
+  return new Promise((resolve, reject) => {
+    jira.search.search({ jql: `project=${constants.project}`, maxResults: 0, fields: [] },
+      function (error, issue) {
+        if (error) {
+          reject(error);
+        }
+        resolve(issue.total);
+      });
   });
+}
+
+function getIssuelinksRelatedOrBlock(issue) {
+  return issue.fields.issuelinks.filter(link => {
+    return link.type.name === 'Related' || link.type.name === 'Block';
+  });
+}
+
+function getExisedIssuelinks(listIssue, listIssuelinks) {
+  const exisedIssuelinks = [];
+  listIssuelinks.forEach((link) => {
+    if (link.inwardIssue) {
+      const linkedIssueIdx = listIssue.findIndex((iss) => iss.id === link.inwardIssue.id);
+      if (linkedIssueIdx > -1) {
+        exisedIssuelinks.push(link);
+      }
+    }
+    if (link.outwardIssue) {
+      const linkedIssueIdx = listIssue.findIndex((iss) => iss.id === link.outwardIssue.id);
+      if (linkedIssueIdx > -1) {
+        exisedIssuelinks.push(link);
+      }
+    }
+  });
+
+  return exisedIssuelinks;
+}
+
+function removeIssueWithEmptyLinks(listIssue) {
+  return listIssue.filter((issue) => {
+    return issue.fields.issuelinks.length > 0;
+  });
+}
+
+router.post('/', function (req, res, next) {
+  countIssues()
+    .then(maxResults => {
+      const searchObject = {
+        issuetype: req.body.issuetype,
+      };
+      
+      jira.search.search({ jql: `project=${constants.project} and issuetype in (${searchObject.issuetype})`, maxResults, fields: constants.fields },
+        function (error, resp) {
+          if (error) {
+            console.error('search', error);
+          }
+
+          const listNewIssue = resp.issues.map((issue) => {
+            const tmpIssuelinks = getIssuelinksRelatedOrBlock(issue);
+            const exisedIssuelinks = getExisedIssuelinks(resp.issues, tmpIssuelinks);
+            const newFields = Object.assign({}, issue.fields, { issuelinks: exisedIssuelinks });
+            const newIssue = Object.assign({}, issue, { fields: newFields });
+
+            return newIssue;
+          });
+
+          const issues = removeIssueWithEmptyLinks(listNewIssue);
+
+          res.send({ issues });
+        });
+    })
+    .catch(err => {
+      console.error('countIssues', err);
+    })
 });
 
 module.exports = router;
